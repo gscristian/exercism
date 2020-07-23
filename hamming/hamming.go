20e2dde1b459498eba32d1eddcaa47642bbaa10c
Cristian Garcia <me@cristian.gs>
2020-07-23 12:31:12 +0200

    Improvements mades to the code based on menthor's feedback

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -1,23 +1,22 @@
 package hamming
 
-import "errors"
+import (
+	"errors"
+)
 
 // Distance function receives two strands of DNA and calculate the hamming distance
 // expects two strings (a,b) and returns an int indicating the hamming distance and an
 // error if something not gone as expected like receiving two strands of different length.
 func Distance(a, b string) (int, error) {
 	counter := 0
-
-	if len(a) != len(b) {
+	ar, br := []rune(a), []rune(b)
+	if len(ar) != len(br) {
 		return 0, errors.New("The length of the strands are different")
-	} else if a == "" || b == "" {
-		return 0, nil
-	} else {
-		for i := 0; i < len(a); i++ {
-			if a[i] != b[i] {
-				counter++
-			}
-		}
-		return counter, nil
 	}
+	for i := range ar {
+		if ar[i] != br[i] {
+			counter++
+		}
+	}
+	return counter, nil
 }
